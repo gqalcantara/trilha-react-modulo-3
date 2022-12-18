34396cc28cef3062457d9f6721846ff247c9a431
Guilherme <gqalcantara@gmail.com>
2022-12-17 22:33:18 -0300

    Finalizado tipagem

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -11,6 +11,7 @@ import { Input } from "../../components/Input";
 import { api } from '../../services/api';
 
 import { Column, Container, CriarText, EsqueciText, Row, SubtitleLogin, Title, TitleLogin, Wrapper } from './styles';
+import { IFormData } from './types';
 
 const schema = yup.object({
     email: yup.string().email('email não é valido').required('Campo obrigatório'),
@@ -21,13 +22,12 @@ const schema = yup.object({
 const Login = () => {
     const navigate = useNavigate();
     
-
-    const { control, handleSubmit, formState: { errors } } = useForm({
+    const { control, handleSubmit, formState: { errors } } = useForm<IFormData>({
         resolver: yupResolver(schema),
         mode: 'onChange',
     });
 
-    const onSubmit = async formData => {
+    const onSubmit = async (formData: IFormData) => {
         try{
             const { data } = await api.get(`users?email=${formData.email}&senha=${formData.password}`);
             if(data.length === 1){
